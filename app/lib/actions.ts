42ab741feaf4986a5f32d803642d068b476860be
salidamaharjan <salidamaharjan@outlook.com>
2024-06-03 11:26:42 -0500

    Learn server validation and show at fornt end

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,9 +6,13 @@ import {redirect} from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0'}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status'
+    }),
     date: z.string(),
 });
 const UpdateInvoice = FormSchema.omit({id: true, date: true});
@@ -39,8 +43,29 @@ export async function updateInvoice(id: string, formData: FormData) {
     }
 }
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+export async function createInvoice(prevState: State, formData: FormData) {
     try {
+        const validatedFields = CreateInvoice.safeParse({
+            customerId: formData.get('customerId'),
+            amount: formData.get('amount'),
+            status: formData.get('status'),
+        })
+        console.log("validatedFields", validatedFields);
+        if (!validatedFields.success) {
+            return {
+                errors: validatedFields.error.flatten().fieldErrors,
+                message: 'Missing Fields. Failed to Create Invoice',
+            }
+        }
         const {customerId, amount, status} = CreateInvoice.parse({
             customerId: formData.get('customerId'),
             amount: formData.get('amount'),
@@ -57,6 +82,8 @@ export async function createInvoice(formData: FormData) {
     } catch (error) {
         console.log({message: error});
     }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
